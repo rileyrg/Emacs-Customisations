6afc7b46be48c10af4b9b38e5c5f08b83873c95e
rileyrg <rileyrg@gmx.de>
2021-05-31 21:55:15 +0200

    early-init

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;;; early-init.el --- early bird  -*- no-byte-compile: t -*-
+(setq package-enabled-at-startup nil)
+(setq load-prefer-newer t)
+(add-to-list 'load-path (expand-file-name "straight/repos/packed" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "straight/repos/auto-compile" user-emacs-directory))
+(require 'auto-compile)
+(auto-compile-on-load-mode)
+(auto-compile-on-save-mode)
+;;; early-init.el ends here
--- init.el
+++ init.el
@@ -40,7 +40,18 @@
 (let ((default-directory elisp-dir))
   (normal-top-level-add-subdirs-to-load-path))
 
-(require 'rgr/early-init "rgr-early-init")
+(defun load-el-gpg (load-dir)
+  (message "attempting mass load from %s." load-dir)
+  (when (file-exists-p load-dir)
+    (dolist (f (directory-files-recursively load-dir "\.[el|gpg]$"))
+      (condition-case nil
+          (progn
+            (message "load-el-gpg loading %s" f)
+            (load f 'no-error))
+        (error nil)))))
+(load-el-gpg (no-littering-expand-etc-file-name "early-load"))
+
+(load-el-gpg (expand-file-name (system-name)  (no-littering-expand-etc-file-name "hosts")))
 
 (require 'rgr/security "rgr-security" 'NOERROR)
 
