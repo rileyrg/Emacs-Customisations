4b7e9e45082d2c9b8ce73b09541b96cad94fbe3c
rileyrg <rileyrg@gmx.de>
2021-07-23 11:00:57 +0200

    added (debug-init) to make it easier to do init debugs

--- early-init.el
+++ early-init.el
@@ -2,8 +2,8 @@
 ;; Maintained in emacs-config.org
 (setq package-enabled-at-startup nil)
 (setq load-prefer-newer t)
-(add-to-list 'load-path (expand-file-name "straight/repos/packed" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "straight/repos/auto-compile" user-emacs-directory))
+;; (add-to-list 'load-path (expand-file-name "straight/repos/packed" user-emacs-directory))
+;; (add-to-list 'load-path (expand-file-name "straight/repos/auto-compile" user-emacs-directory))
 (require 'auto-compile)
 (auto-compile-on-load-mode)
 (auto-compile-on-save-mode)
