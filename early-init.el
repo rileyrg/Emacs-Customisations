876e8360844eee8468c40b37c87c4832299e55f9
rileyrg <rileyrg@gmx.de>
2021-08-06 11:41:32 +0200

    remove auto-compile completely. straight.el doe it all.

--- early-init.el
+++ early-init.el
@@ -1,11 +1,4 @@
 ;;; early-init.el --- early bird  -*- no-byte-compile: t -*-
 ;; Maintained in emacs-config.org
 (setq package-enabled-at-startup nil)
-;; (setq load-prefer-newer t)
-;; ;; (add-to-list 'load-path (expand-file-name "straight/repos/packed" user-emacs-directory))
-;; ;; (add-to-list 'load-path (expand-file-name "straight/repos/auto-compile" user-emacs-directory))
-;; (use-package auto-compile
-;;   :init
-;; (auto-compile-on-load-mode)
-;; (auto-compile-on-save-mode))
-  ;;; early-init.el ends here
+;;; early-init.el ends here
