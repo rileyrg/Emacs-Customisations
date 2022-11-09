96a5fb3fe1494dc86082464da140415414da4677
rileyrg <rileyrg@gmx.de>
2022-11-09 09:43:00 +0100

    remove blacken and prettifier-js for Apheleia, add tree-sitter

--- etc/elisp/rgr-programming.el
+++ etc/elisp/rgr-programming.el
@@ -44,13 +44,33 @@
 (global-set-key (kbd "S-<f2>") 'display-line-numbers-mode)
 (add-hook 'prog-mode-hook (lambda() (display-line-numbers-mode t)))
 
-(use-package rainbow-delimiters
+(use-package tree-sitter
+  :ensure t
   :config
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
-(use-package rainbow-identifiers
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
+;; auto-format different source code files extremely intelligently
+;; https://github.com/radian-software/apheleia
+(use-package apheleia
+  :ensure t
   :config
-  (add-hook 'prog-mode-hook #'rainbow-identifiers-mode))
+  (apheleia-global-mode +1))
+
+(use-package rainbow-delimiters
+      :config
+      (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+    (use-package rainbow-identifiers
+      :config
+      (add-hook 'prog-mode-hook #'rainbow-identifiers-mode))
 
 (require 'project)
 
@@ -135,14 +155,6 @@
   (add-to-list 'auto-mode-alist '("\\.js?\\'" . rjsx-mode))
   (add-to-list 'auto-mode-alist '("\\.ts\\'" . rjsx-mode))
   )
-
-(use-package prettier-js
-  :custom
-  (prettier-js-args '(
-                      "--trailing-comma" "all"
-                      "--bracket-spacing" "false"
-                      "--print-width" "80"
-                      )))
 
 (defun rgr/js-mode-hook ()
   (setq-local js-indent-level 2)
@@ -248,13 +260,11 @@
   (use-package  lsp-haskell)
   (add-hook 'haskell-mode-hook #'lsp-deferred)
   (add-hook 'haskell-literate-mode-hook #'lsp-deferred)
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode
-            (eval-after-load "haskell-mode"
-              '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
-
-            (eval-after-load "haskell-cabal"
+  (eval-after-load "haskell-mode"
+    '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
+  (eval-after-load "haskell-cabal"
               '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
-            ))
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
 
 (use-package lldb-voltron
   :straight (lldb-voltron :local-repo "~/development/projects/emacs/emacs-lldb-voltron" :type git :host github :repo "rileyrg/emacs-lldb-voltron" ))
