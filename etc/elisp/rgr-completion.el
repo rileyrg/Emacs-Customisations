79caa83ebcaa44bca299ece5751fec59e8451925
rileyrg <rileyrg@gmx.de>
2023-07-23 00:11:49 +0200

    corfu lsp

--- etc/elisp/rgr-completion.el
+++ etc/elisp/rgr-completion.el
@@ -11,25 +11,7 @@
 (use-package corfu
   ;; Optional customizations
   :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
   (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; Enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since Dabbrev can be used globally (M-/).
-  ;; See also `corfu-excluded-modes'.
   :init
   (global-corfu-mode))
 
--- etc/elisp/rgr-lsp.el
+++ etc/elisp/rgr-lsp.el
@@ -1,3 +1,24 @@
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-l")
+  :config
+  (use-package lsp-ui :commands lsp-ui-mode)
+  (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+  (lsp-treemacs-sync-mode 1)
+  (use-package flycheck)
+  (use-package dap-mode)
+  (use-package lsp-dart)
+  (setq lsp-completion-provider :capf)
+  ;; (defun corfu-lsp-setup ()
+  ;;   (setq-local completion-styles '(orderless)
+  ;;               completion-category-defaults nil))
+  ;; (add-hook 'lsp-completion-mode-hook #'corfu-lsp-setup)
+
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deferred))
+
 (use-package eglot
   :straight `(eglot ,@(when (>= emacs-major-version 29) '(:type built-in)))
   :config
