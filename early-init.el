eeb659543b63bd58711bb4dfc7ebdcef8fcd85ea
rileyrg <rileyrg@gmx.de>
2023-11-01 22:21:43 +0100

    change eln-cache-dir location

--- early-init.el
+++ early-init.el
@@ -2,4 +2,7 @@
 ;; Maintained in emacs-config.org
 (setq package-enabled-at-startup nil)
 (setq max-specpdl-size 13000)
+;; Set eln-cache dir
+(when (boundp 'native-comp-eln-load-path)
+  (startup-redirect-eln-cache (expand-file-name "var/eln-cache" user-emacs-directory)))
 ;;; early-init.el ends here
