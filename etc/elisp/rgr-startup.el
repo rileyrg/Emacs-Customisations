4decff17c9526cf397c4bc748b16b5e7ccfa9743
rileyrg <rileyrg@gmx.de>
2023-11-09 16:06:24 +0100

    save-place and startup

--- etc/elisp/rgr-startup.el
+++ etc/elisp/rgr-startup.el
@@ -1,13 +1,23 @@
 (use-package emacs
-  :custom
-  (backup-directory-alist
-        `(("." . ,(concat user-emacs-directory "backups"))))
-  (desktop-path '("~/.emacs.d/var/desktop"))
-  (desktop-save t)
-  (desktop-load-locked-desktop t)
-  (desktop-restore-frameset nil)
-  (desktop-restore-eager  10)
+
+  :bind
+  (("C-c x" . rgr/quit-or-close-emacs))
+
+  :init
+  (recentf-mode 1)
+  (add-to-list 'recentf-exclude no-littering-var-directory)
+  (add-to-list 'recentf-exclude no-littering-etc-directory)
+  (add-to-list 'recentf-exclude "~/.pub-cache")
+  (require 'saveplace)
+  (save-place-mode t)
+  (require 'savehist)
+  (add-to-list 'savehist-additional-variables 'kill-ring)
+  (add-to-list 'savehist-additional-variables 'global-mark-ring)
+  (add-to-list 'savehist-ignored-variables 'file-name-history)
+  (savehist-mode 1)
+
   :config
+
   (defun rgr/quit-or-close-emacs(&optional kill)
     (interactive)
     (if (or current-prefix-arg kill)
@@ -21,41 +31,6 @@
     ;;(savehist-save)
     (save-buffers-kill-emacs))
 
-  (use-package saveplace
-    :config
-    (save-place-mode t))
-
-  (savehist-mode 1)
-  (add-to-list 'savehist-additional-variables 'kill-ring)
-  (add-to-list 'savehist-additional-variables 'global-mark-ring)
-  (add-to-list 'savehist-ignored-variables 'file-name-history)
-
-  (recentf-mode 1)
-  (add-to-list 'recentf-exclude no-littering-var-directory)
-  (add-to-list 'recentf-exclude no-littering-etc-directory)
-  (add-to-list 'recentf-exclude "~/.pub-cache")
-
-  (desktop-save-mode -1) ;; TODO - doesnt work with lsp-deferred
-
-  (use-package psession
-    :disabled
-    :init
-    (psession-mode 1)
-    (psession-autosave-mode 1)
-    (psession-savehist-mode 1))
-
-  (global-set-key (kbd "C-c x") 'rgr/quit-or-close-emacs)
   )
-
-;; start emacs-server if not running
-;; problems in emacs 29 - temporarily stopped
-;; (unless(daemonp)
-;;   (add-hook 'after-init-hook
-;;             (lambda ()
-;;               (require 'server)
-;;               (unless (server-running-p)
-;;                 (message "Starting EmacsServer from init as not already running.")
-;;                 (server-start))
-;;               )))
 
 (provide 'rgr/startup)
