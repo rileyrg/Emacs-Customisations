1b9f4cdf3bb1a01e18ddb5eb7f5c7acb8b25e974
rileyrg <rileyrg@gmx.de>
2023-12-15 15:07:41 +0100

    give up desktop mode with daemon

--- etc/elisp/rgr-startup.el
+++ etc/elisp/rgr-startup.el
@@ -1,15 +1,15 @@
-(use-package emacs
+(recentf-mode 1)
+(savehist-mode 1)
+(save-place-mode 1)
 
-  :bind
-  (("C-c x" . rgr/quit-or-close-emacs))
+  ;; (defun rgr/startup-hook (f)
+  ;;   (desktop-save-mode)
+  ;;   (desktop-read)
+  ;;   )
 
-  :init
-
-  ;(recentf-mode 1)
-  ;(savehist-mode 1)
-  ;(save-place-mode 1)
-  (desktop-save-mode 1)
-  (add-to-list 'after-make-frame-functions '(lambda()((desktop-save-mode 1))))
+  ;; (add-hook 'desktop-after-read-hook (lambda()(consult-buffer)))
+  ;; (add-hook 'after-make-frame-functions #'rgr/startup-hook)
+  ;; ;;(add-hook 'server-switch-hook #'rgr/startup-hook)
 
   (defun rgr/quit-or-close-emacs(&optional kill)
     (interactive)
@@ -24,6 +24,6 @@
     ;;(savehist-save)
     (save-buffers-kill-emacs))
 
-  )
+  (global-set-key (kbd "C-c x") 'rgr/quit-or-close-emacs)
 
 (provide 'rgr/startup)
