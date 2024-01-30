91b4ab4cc72b1785f870d224e601b4d09e175c38
rileyrg <rileyrg@gmx.de>
2024-01-30 09:57:07 +0100

    changes to rgr/kill-dwim just to try mapc ;)

--- etc/elisp/rgr-kill-dwim.el
+++ etc/elisp/rgr-kill-dwim.el
@@ -1,35 +1,39 @@
 (use-package emacs
-   :init
-   (setq thing-at-point-provider-alist (append thing-at-point-provider-alist
-           '((kill-dwim . rgr/thing-at-point-kill-dwim))))
+  :init
+  (setq thing-at-point-provider-alist (append thing-at-point-provider-alist
+                                              '((rgr/tap-kdwim . rgr/tap-kill-dwim)
+                                                (rgr/tap-region . rgr/get-region))))
 
- :config
- (defun rgr/get-region()
-   "return string in region if selected and deactivate, else nil"
-   (if(use-region-p)
-       (let ((txt (buffer-substring-no-properties
-                   (mark)
-                   (point))))
-         (deactivate-mark)
-         txt)
-     nil))
+  :config
+  (defcustom rgr/kill-dwim-tap-symbols '(rgr/tap-region url filename email symbol sexp word line)
+    "`thing-at-point' candidates for killing")
 
- (defun rgr/thing-at-point-kill-dwim()
-   (or (rgr/get-region) (thing-at-point 'url) (thing-at-point 'filename) (thing-at-point 'email) (thing-at-point 'symbol) (thing-at-point 'sexp) (thing-at-point 'word) ))
+  (defun rgr/get-region()
+    "return string in region if selected and deactivate, else nil"
+    (if(use-region-p)
+        (let ((txt (buffer-substring-no-properties
+                    (mark)
+                    (point))))
+          (deactivate-mark)
+          txt)
+      nil))
 
- (defun rgr/kill-dwim ()
-   "work out what to pick up from point and stick in the kill ring"
-   (interactive)
-   (let ((s (thing-at-point 'kill-dwim)))
-     (condition-case nil
-         (message s)
-       (error nil))
-     "if a prefix argument (4)(C-u) allow edit / read from input"
-     (if current-prefix-arg
-         (setq s (read-string "text:" s)))
-     (when s
-       (kill-new s))))
-:bind
- ("M-w" . #'rgr/kill-dwim))
+  (defun rgr/tap-kill-dwim()
+    (catch 'found
+      (mapcar (lambda(x)
+                (let ((v (thing-at-point x)))
+                  (when v (throw 'found v)))) rgr/kill-dwim-tap-symbols)))
+
+  (defun rgr/kill-dwim ()
+    "work out what to pick up from point and stick in the kill ring"
+    (interactive)
+    (let ((s (thing-at-point 'rgr/tap-kdwim)))
+      (if current-prefix-arg
+          (setq s (read-string "text:" s)))
+      (when s
+        (message "%s" s)
+        (kill-new s))))
+  :bind
+  ("M-w" . #'rgr/kill-dwim))
 
 (provide 'rgr/kill-dwim)
