1ddd87f092b45eeb8bf8d7fae011a19fdc1f3f47
rileyrg <rileyrg@gmx.de>
2025-01-11 08:21:55 +0100

    revert  mode tweek https://www.reddit.com/r/emacs/comments/10he3fc/globalautorevertmode_stopped_working/

--- etc/elisp/rgr-general-config.el
+++ etc/elisp/rgr-general-config.el
@@ -19,6 +19,7 @@
   ;; Also auto refresh dired, but be quiet about it
   (setq global-auto-revert-non-file-buffers t)
   (setq auto-revert-verbose nil)
+  (setq auto-revert-use-notify nil)
 
   (global-visual-line-mode 1)
 
